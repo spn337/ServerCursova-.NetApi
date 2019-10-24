f2c7458ab904f5f48d2ea384074a27b4f20654d1
spn337 <spn337@gmail.com>
2019-10-24 11:12:49 +0300

    Add ProductController(Get and Post)

---
+++ WebServerCursova/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using WebServerCursova.Entities;
+using WebServerCursova.ViewModels;
+
+namespace WebServerCursova.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly EFDbContext _context;
+        //доступ до файла app.setting
+        private readonly IConfiguration _configuration;
+        //отримати доступ до сервера
+        private readonly IHostingEnvironment _env;
+
+        public ProductController(IHostingEnvironment env, IConfiguration configuration, EFDbContext context)
+        {
+            _configuration = configuration;
+            _env = env;
+            _context = context;
+        }
+
+        #region HttpGET
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var model = _context.Products.Select(
+                p => new ProductGetVM
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price
+                }).ToList();
+
+            return Ok(model);
+        }
+        #endregion
+
+        #region HttpPOST
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        public IActionResult Create([FromBody]ProductPostVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            // передаємо модель в БД
+            DbProduct p = new DbProduct
+            {
+                Name = model.Name,
+                Price = model.Price,
+                DateCreate = DateTime.Now
+            };
+            _context.Products.Add(p);
+            _context.SaveChanges();
+
+            return Ok(p.Id);
+        }
+        #endregion
+
+        #region HttpDELETE
+        [HttpDelete]
+        public IActionResult Delete([FromBody]ProductDeleteVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var prod = _context.Products.SingleOrDefault(p => p.Id == model.Id);
+            if (prod != null)
+            {
+                _context.Products.Remove(prod);
+                _context.SaveChanges();
+            }
+            return Ok(prod.Id);
+        }
+        #endregion
+    }
+}
---
+++ WebServerCursova/ViewModels/ProductVM.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebServerCursova.ViewModels
+{
+    public class ProductGetVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductPostVM
+    {
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public decimal Price { get; set; }
+    }
+
+    public class ProductDeleteVM
+    {
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public int Id { get; set; }
+    }
+
+    public class ProductPutVM
+    {
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле не може бути порожнім")]
+        public decimal Price { get; set; }
+    }
+}
